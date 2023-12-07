a6460c9bfcb13a0c0a3b945d2b49bd866147b563
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-07 16:31:30 -0300

    ardd

--- src/components/templates/AppbarCabecalho.tsx
+++ src/components/templates/AppbarCabecalho.tsx
@@ -1,11 +1,34 @@
-export default function Appbar() {
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Titulobash from './Titulobash';
 
+export default function ButtonAppBar() {
     return (
-        <>
-        <Appbar
-        
-        />
-        </>
-    )
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        onClick={() => { }}
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
 
+                    <Titulobash titulo='titulos' subtitulo='sub' />
+
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
--- src/components/templates/Titulobash.tsx
+++ src/components/templates/Titulobash.tsx
@@ -1,11 +1,20 @@
 import Typography from "@mui/material/Typography";
+interface TitulobashProps {
+    titulo: string;
+    subtitulo: string;
+    children?: React.ReactNode;
+}
 
-export default function Titulobash(props: any) {
+
+export default function Titulobash(props: TitulobashProps) {
 
     return (
         <>
-            <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                {props.children}
+            <Typography component="h1" variant="h1" color="primary" gutterBottom>
+                {props.titulo}
+            </Typography>
+            <Typography component="h1" variant="h2" color="primary" gutterBottom>
+                {props.subtitulo}
             </Typography>
         </>
     )
